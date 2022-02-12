c79e8d96b7f1c38e8c010a621a55e5049537f5d5
Huy Ho <ho.an.huy@live.com>
2022-02-13 03:07:27 +0700

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+using JWT.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWT.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase{
+
+    public static User user = new User();
+    private readonly IConfiguration _configuration = null;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(UserDto request)
+    {
+        user.Username = request.Username;
+        user.Password = request.Password; 
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(UserDto  requesst)
+    {
+        if (user.Username != requesst.Username)
+        {
+            return BadRequest("User not found");
+        }
+        var token = CreateToken(user);
+
+        return Ok(token);
+    }
+    private string CreateToken(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+        };
+        var  key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            _configuration.GetSection("AppSettings:Token").Value));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds);
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        return jwt;
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,13 @@
+﻿namespace JWT.Model
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
